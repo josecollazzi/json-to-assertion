1befa6c2a06608c69657bff55dc76ff7d12e2fab
Jose Collazzi <josecollazzi@gmail.com>
2019-04-01 17:39:03 +0100

    Updates

---
+++ precache-manifest.fa8e8f4ba58a90a5f30145938573365c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f4cf1b63471ff717e1",
+    "url": "/json-to-assertion/static/css/main.03d456f4.chunk.css"
+  },
+  {
+    "revision": "f1f4cf1b63471ff717e1",
+    "url": "/json-to-assertion/static/js/main.4ad32458.chunk.js"
+  },
+  {
+    "revision": "d3df154103ceaf9531d3",
+    "url": "/json-to-assertion/static/js/runtime~main.883e21a2.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/json-to-assertion/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "62e5311011dcb800383505749e7fce06",
+    "url": "/json-to-assertion/index.html"
+  }
+];
