5ddf386bc5e5fc9b1b40e4fbb49ec3afe891d2bc
Jose Collazzi <josecollazzi@gmail.com>
2019-04-01 17:27:36 +0100

    first commit

--- README.md
+++ README.md
@@ -66,3 +66,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## License
+
+This is released under the MIT License
--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: '{}'};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+
+  printEnd = (path, value) => {
+    if (value === null) {
+      return '.body("' + path + '", equalTo(nullValue()))';
+    }
+
+    let preparedValue = value;
+
+    if(typeof value === "string") {
+      preparedValue = "\"" + value + "\"";
+    }
+
+    return '.body("' + path + '", equalTo(' + preparedValue + '))';
+  };
+
+  printArray = (path, jsonArray) => {
+    if (path === null || path === "undefined" || path === "") {
+      path = "";
+    }
+
+    return jsonArray.map((value, index) => {
+      console.log(value);
+      return <div>
+        {this.print(path + "[" + index + "]", value)}
+      </div>
+    })
+  };
+
+  printObject = (path, jsonObject) => {
+    if (path === null || path === "undefined" || path === "") {
+      path = "";
+    } else {
+      path = path + ".";
+    }
+    return Object.keys(jsonObject).map((key) =>
+        <div>
+          {this.print(path + key,jsonObject[key])}
+        </div>)
+  };
+
+  print = (path, value) => {
+    if (this.typeElement(value) === "array") {
+      return this.printArray(path, value);
+    } else if (this.typeElement(value) === "object") {
+      return this.printObject(path, value);
+    } else {
+      return this.printEnd(path, value);
+    }
+  };
+
+  typeElement = (object) => {
+    if ( Array.isArray(object)) {
+      return "array";
+    } else if (object === null || object === undefined) {
+      return "null";
+    }else {
+      return typeof object;
+    }
+  };
+
+  /**
+   typeof undefined // "undefined"
+   typeof 0 // "number"
+   typeof true // "boolean"
+   typeof "foo" // "string"
+   typeof Symbol("id") // "symbol"
+   typeof Math // "object"  (1)
+   typeof null // "object"  (2)
+   typeof alert // "function"  (3)
+   */
+
   render() {
+    var list = null;
+    var jsonObject = null
+    try {
+
+     jsonObject = JSON.parse(this.state.value);
+     list = this.print("", jsonObject);
+    } catch (e) {
+      list = <div>not valid json</div>
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <label>
+            Name:
+            <input type="textarea" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+        <div>{list}</div>
       </div>
     );
   }
